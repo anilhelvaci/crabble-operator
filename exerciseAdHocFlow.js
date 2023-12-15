a2c6aefce34e50d236f4de51ef6b9022a5917e10
JorgeLopes-BytePitch <jorge.lopes@bytepitch.com>
2023-12-15 18:24:20 +0000

    Add method to retrieve rentalHandles

--- exerciseAdHocFlow.js
+++ exerciseAdHocFlow.js
@@ -14,10 +14,16 @@ const exerciseAdHocFlow = async () => {
     instances: rawInstances,
     brands: rawBrands,
     issuers: rawIssuers,
+    rentals: rawRentals,
   } = await getState();
   const instances = Object.fromEntries(rawInstances);
   const brands = Object.fromEntries(rawBrands);
   const issuers = Object.fromEntries(rawIssuers);
+  const rentals = Object.fromEntries(rawRentals);
+
+  const getRentalHandle = (index) => {
+    return rentals[`rental${index}`].rentalHandle;
+  };
 
   const crabbleOffers = makeCrabbleFlowOffers(
     offerSender,
@@ -29,7 +35,7 @@ const exerciseAdHocFlow = async () => {
   console.log("Exercise Ad-Hoc Create Rental...");
 
   crabbleOffers.createAdHocRental("createRental-1", "gov1");
-  crabbleOffers.buyOut("buyout-1", "!!!!!rentalHandle", "gov1");
+  crabbleOffers.buyOut("buyout-1", getRentalHandle(1), "gov1");
   crabbleOffers.withdrawUtility("withdraw-utility-1", "createRental-1", "gov1");
   crabbleOffers.withdrawCollateral(
     "withdraw-collateral-1",
@@ -42,8 +48,10 @@ const exerciseAdHocFlow = async () => {
     "gov1"
   );
   crabbleOffers.createAuctionRental("createRental-2", "gov1");
-  crabbleOffers.bid("bid-1", "!!!!!rentalHandle", "gov1");
+  crabbleOffers.bid("bid-1", getRentalHandle(2), "gov1");
   crabbleOffers.acceptBid("acceptBid-1", "createRental-2", "bid-1", "gov1");
+  crabbleOffers.borrow("borrow-1", "bid-1", "gov1");
+  crabbleOffers.returnUtility("adhoc-return-1", "buyout-1", "gov1");
 };
 
 exerciseAdHocFlow().then(() => process.exit(0));
